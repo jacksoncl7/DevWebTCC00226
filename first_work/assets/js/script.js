5d51e0222333a6c064b9394f13424811b21f8f2b
jacksoncl7 <jacksonlima@id.uff.br>
2019-04-09 15:53:11 -0300

    modificacaoes na index.html
    adicionado o carousel, alert no card paraty e o scroll-spy [foi necessário adicionar uma
    funcacao JQuery]

--- first_work/assets/js/script.js
+++ first_work/assets/js/script.js
@@ -1,22 +1,24 @@
 $(function() {
-   // $('[data-toggle="tooltip"]').tooltip({ boundary: 'window' });
+    $('[data-toggle="tooltip"]').tooltip({ boundary: 'window' });
 
-   // $('[data-toggle="popover"]').popover();
+    $('[data-toggle="popover"]').popover();
 
-   // $('a.nav-link.nav-item').mouseenter(function() {
-   //    $('a.nav-link.nav-item').removeClass('active');
-   //    $(this).addClass('active');
-   // });
+    $('a.nav-link.nav-item').mouseenter(function() {
+       $('a.nav-link.nav-item').removeClass('active');
+       $(this).addClass('active');
+    });
 
-   // $('a.nav-link.nav-item').not(".dropdown-toggle").click(function() {
-   //    $('div.collapse.navbar-collapse').removeClass('show');
-   // });
-   
-   // $('a.dropdown-item').click(function() {
-   //    $('div.collapse.navbar-collapse').removeClass('show');
-   // });
-   
-   /* $('a.nav-link.nav-item').mouseleave(function() {
+    $('a.nav-link.nav-item').not(".dropdown-toggle").click(function() {
+       $('div.collapse.navbar-collapse').removeClass('show');
+    });
+
+    $('a.dropdown-item').click(function() {
+       $('div.collapse.navbar-collapse').removeClass('show');
+    });
+
+    $('a.nav-link.nav-item').mouseleave(function() {
          $(this).removeClass('active');
-      }); */
+      });
 });
+//JQuery var scrolling used JS, specify scrollspy from bootstrap.js
+$('body').scrollspy()
