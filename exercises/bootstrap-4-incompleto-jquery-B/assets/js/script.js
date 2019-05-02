d378d511286a9725da5224fcab135fb033499ea8
jacksoncl7 <jacksonlima@id.uff.br>
2019-05-02 00:13:55 -0300

    add any classroom  exercises

---
+++ exercises/bootstrap-4-incompleto-jquery-B/assets/js/script.js
@@ -0,0 +1,27 @@
+/// <reference path="../../typings/globals/jquery/index.d.ts" />
+
+$(function () {
+   $('[data-toggle="tooltip"]').tooltip({ boundary: 'window' });
+
+   $('[data-toggle="popover"]').popover();
+
+   $("#btn7").click(function() {
+
+   })
+
+   $("#btn8").click(function() {
+
+   })
+
+   $("#btn9").click(function() {
+
+   })
+
+   $("#btn10").click(function() {
+
+   })
+
+   $("#btn11").click(function() {
+
+   })
+});
