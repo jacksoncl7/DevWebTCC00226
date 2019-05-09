4279f4cb324c42f609fccee63fa1a2078e386610
jacksoncl7 <jacksonlima@id.uff.br>
2019-05-09 08:24:52 -0300

    inicio de segundo projeto quarto trabalho da disciplina - feitas modificacoes incluisoes com html ja estruturado e decorado

---
+++ second_work/assets/js/script.js
@@ -0,0 +1,20 @@
+$(function() {
+  
+  // 18:15 SPA UFF
+  // $('a.nav-link.nav-item').mouseenter(function() {
+  //    $('a.nav-link.nav-item').removeClass('active');
+  //    $(this).addClass('active');
+  // });
+  //
+  // $('a.nav-link.nav-item').not(".dropdown-toggle").click(function() {
+  //    $('div.collapse.navbar-collapse').removeClass('show');
+  // });
+  //
+  // $('a.dropdown-item').click(function() {
+  //    $('div.collapse.navbar-collapse').removeClass('show');
+  // });
+  //
+  // $('a.nav-link.nav-item').mouseleave(function() {
+  //      $(this).removeClass('active');
+  //   });
+});
